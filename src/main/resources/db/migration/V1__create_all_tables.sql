ddb0b0cde33ee7ab00d4a7f7a9e4b8f93160d14f
carlinhos12345678962 <carlinhos12345678962@gmail.com>
2025-05-05 01:19:01 +0000

    Criando tabelas em um script apenas

--- src/main/resources/db/migration/V1__create_all_tables.sql
+++ src/main/resources/db/migration/V1__create_all_tables.sql
@@ -1,3 +1,4 @@
+-- V1__create_all_tables.sql
 -- Criação da tabela Cliente
 CREATE TABLE cliente (
                          id BIGSERIAL PRIMARY KEY,
@@ -10,18 +11,18 @@ CREATE TABLE cliente (
 CREATE TABLE endereco (
                           id BIGSERIAL PRIMARY KEY,
                           rua VARCHAR(100) NOT NULL,
-                          numero VARCHAR(10) NOT NULL,
+                          numero VARCHAR(255) NOT NULL,
                           cidade VARCHAR(50) NOT NULL,
                           estado VARCHAR(2) NOT NULL,
-                          cep VARCHAR(8) NOT NULL,
+                          cep INT NOT NULL,
                           cliente_id BIGINT NOT NULL,
                           FOREIGN KEY (cliente_id) REFERENCES cliente(id)
 );
 
--- Criação da tabela Conta - Ajuste no nome da coluna id_cliente para cliente_id
+-- Criação da tabela Conta
 CREATE TABLE conta (
                        id BIGSERIAL PRIMARY KEY,
-                       numero_conta VARCHAR(20) UNIQUE NOT NULL,
+                       numero_conta VARCHAR(255) UNIQUE NOT NULL,
                        saldo DECIMAL(15, 2) NOT NULL DEFAULT 0.00,
                        tipo VARCHAR(20) NOT NULL,
                        cliente_id BIGINT NOT NULL,
@@ -35,6 +36,28 @@ CREATE TABLE transferencia (
                                valor DECIMAL(15, 2) NOT NULL,
                                id_conta_origem INT NOT NULL,
                                id_conta_destino INT NOT NULL,
-                               FOREIGN KEY (id_conta_origem) REFERENCES conta(id_conta),
-                               FOREIGN KEY (id_conta_destino) REFERENCES conta(id_conta)
+                               FOREIGN KEY (id_conta_origem) REFERENCES conta(id),
+                               FOREIGN KEY (id_conta_destino) REFERENCES conta(id)
+);
+
+CREATE TABLE usuario (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+-- Adicionar coluna id_usuario na tabela cliente como chave estrangeira referenciando a tabela usuario
+ALTER TABLE cliente
+ADD COLUMN usuario_id INT REFERENCES usuario(id);
+
+-- Adicionar coluna id_usuario na tabela conta como chave estrangeira referenciando a tabela usuario
+ALTER TABLE conta
+ADD COLUMN usuario_id INT REFERENCES usuario(id);
+
+CREATE TABLE log (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    data_hora TIMESTAMP NOT NULL,
+    descricao TEXT NOT NULL,
+    usuario_id INT,
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
--- src/main/resources/db/migration/V2__alter_endereco_and_conta_columns.sql
+++
@@ -1,9 +0,0 @@
--- Alterar a coluna numero de Endereco para VARCHAR
-ALTER TABLE endereco ALTER COLUMN numero TYPE VARCHAR(255);
-
--- Alterar a coluna cep de Endereco para INT
-ALTER TABLE endereco
-ALTER COLUMN cep TYPE INT USING cep::integer;
-
--- Alterar a coluna numero de Conta para VARCHAR
-ALTER TABLE conta ALTER COLUMN numero TYPE VARCHAR(255);
