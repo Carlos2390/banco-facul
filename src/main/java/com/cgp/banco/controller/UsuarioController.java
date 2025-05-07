982299c7443176de7bd0e790fe5f25cdeca0ec30
carlos2390 <carlinhos12345678962@gmail.com>
2025-05-07 20:22:34 -0300

    Aceitando requisição de qualquer lugar no UsuarioController

--- src/main/java/com/cgp/banco/controller/UsuarioController.java
+++ src/main/java/com/cgp/banco/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/usuario")
 public class UsuarioController {
 
