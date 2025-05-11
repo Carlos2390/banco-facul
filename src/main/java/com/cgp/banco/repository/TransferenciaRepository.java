fb704fd53481a949574bc52c17335a489b228f92
carlos2390 <carlinhos12345678962@gmail.com>
2025-05-11 20:01:28 -0300

    Corrrigido nome do metodo findByContaDestinoNumeroContaOrContaOrigemNumeroConta

--- src/main/java/com/cgp/banco/repository/TransferenciaRepository.java
+++ src/main/java/com/cgp/banco/repository/TransferenciaRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface TransferenciaRepository extends JpaRepository<Transferencia, Long> {
     List<Transferencia> findByIdContaDestino(Long idContaDestino);
     List<Transferencia> findByIdContaOrigem(Long idContaOrigem);
-    List<Transferencia> findByNumeroContaDestinoOrNumeroContaOrigem(String numeroContaDestino, String numeroContaOrigem);
+    List<Transferencia> findByContaDestinoNumeroContaOrContaOrigemNumeroConta(String numeroContaDestino, String numeroContaOrigem);
 }
